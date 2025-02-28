aafcc2281bce610d4a5dc0a418b50031bc30fee3
AllanFGR <115052566+Allan-Figueiredo@users.noreply.github.com>
2025-02-28 18:03:10 -0300

    Update index.js

--- Questao - 2/index.js
+++ Questao - 2/index.js
@@ -1,18 +1,20 @@
 function compactarString(str) {
-    let resultado = '';
-    let contador = 1;
+    let resultado = '';  // Variável para armazenar o resultado final da string compactada
+    let contador = 1;  // Variável para contar a quantidade de caracteres repetidos
 
+    // Loop para percorrer
     for (let i = 0; i < str.length; i++) {
         
+        // Se o caractere atual for igual ao próximo, incrementa o contador
         if (str[i] === str[i + 1]) {
             contador++;
         } else {
-            
+            // Quando os caracteres não forem iguais, adiciona o caractere e a quantidade no resultado
             resultado += str[i] + contador;
-            contador = 1;
+            contador = 1; // Reseta o contador para o próximo caractere
         }
     }
-    return resultado;
+    return resultado;  // Retorna a string compactada
 }
 
 console.log(compactarString("jjjjooaoo"));
